6407b80c0159aa9781368f1466fdb5afede398b2
Qi Hangkong <qihangkong@outlook.com>
2023-05-10 14:16:39 +0800

    rvgpu: add interface vkCmdCopyImageToBuffer2

--- src/rvgpu/vulkan/rvgpu_cmd_buffer.c
+++ src/rvgpu/vulkan/rvgpu_cmd_buffer.c
@@ -99,3 +99,18 @@ rvgpu_CmdPipelineBarrier2(VkCommandBuffer commandBuffer,
 {
    RVGPU_FROM_HANDLE(rvgpu_cmd_buffer, cmd_buffer, commandBuffer);
 }
+
+VKAPI_ATTR void VKAPI_CALL
+rvgpu_CmdCopyImageToBuffer2(VkCommandBuffer commandBuffer,
+                            const VkCopyImageToBufferInfo2 *pCopyImageToBufferInfo)
+{
+   RVGPU_FROM_HANDLE(rvgpu_cmd_buffer, cmd_buffer, commandBuffer);
+   RVGPU_FROM_HANDLE(rvgpu_image, src_image, pCopyImageToBufferInfo->srcImage);
+   RVGPU_FROM_HANDLE(rvgpu_buffer, dst_buffer, pCopyImageToBufferInfo->dstBuffer);
+
+   for (unsigned r = 0; r < pCopyImageToBufferInfo->regionCount; r++) {
+      // copy_image_to_buffer(cmd_buffer, dst_buffer, src_image,
+      //                     pCopyImageToBufferInfo->srcImageLayout,
+      //                     &pCopyImageToBufferInfo->pRegions[r]);
+   }
+}
