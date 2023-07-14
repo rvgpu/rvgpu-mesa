4467c551ff9187284a51d865f2bc1fd0cd00b91e
Qi Hangkong <qihangkong@outlook.com>
2023-07-14 16:51:34 +0800

    feat: add function rc_init_llvm_once

---
+++ src/rvgpu/vulkan/rvgpu_shader.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2023 Sietium Semiconductor
+ *
+ * based in part on radv driver which is:
+ * Copyright © 2016 Red Hat.
+ * Copyright © 2016 Bas Nieuwenhuizen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "rc_llvm_util.h"
+
+#include "rvgpu_private.h"
+
+void *
+rvgpu_shader_compile(struct rvgpu_device *device, struct rvgpu_shader *shader, nir_shader *nir)
+{
+   printf("[NIR TO Binary]\n");
+   nir_print_shader(nir, stdout);
+ 
+   rc_init_llvm_once();
+
+   return NULL;
+}
+
+
